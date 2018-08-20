d5137ead5c4cf5e1c49e4707b6501423b0961fd3
Sam <SSUrbancic@outlook.com>
2018-08-20 13:08:16 -0500

    spotify api
    added a spotify js as welll as spotify keys and an index view for the future implementation of the spotify api.

--- BandPromotionPlatform/Keys.cs
+++ BandPromotionPlatform/Keys.cs
@@ -11,5 +11,7 @@ namespace BandPromotionPlatform
         public const string ClientID = "66888775037-sd5610o088oel7eheujq6v8gidc1tjvr.apps.googleusercontent.com";
         public const string YoutubeClientSecret = "Qmn1sUh7A9zK1wNBjHr2PGwH";
         public const string YoutubeApiKey = "AIzaSyDnbwroZ7fQonrCpoxq43OeA3dYYuo1h1E";
+        public const string SpotifyClientSecret = "c75a9f8a7d7c479aaf0a88496e4a5711";
+        public const string SpotifyClientID = "ef393695fcff479390c288dcd69a1a8b";
     }
 }
---
+++ BandPromotionPlatform/Spotify.js
@@ -0,0 +1,22 @@
+﻿var Spotify = require('node-spotify-api');
+var 
+var spotify = new Spotify({
+    id: "c75a9f8a7d7c479aaf0a88496e4a5711",
+    secret: "ef393695fcff479390c288dcd69a1a8b" >
+    });
+spotify
+    .search({ type: 'track', query: 'All the Small Things' })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+spotify
+    .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (err) {
+        console.error('Error occurred: ' + err);
+    });
