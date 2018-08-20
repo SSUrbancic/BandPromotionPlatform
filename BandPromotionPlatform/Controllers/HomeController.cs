bb8d9ee4ead3369f8e85870e566d8ebcc40ebc13
Sam <SSUrbancic@outlook.com>
2018-08-20 10:45:52 -0500

    SoundCloud
    Was able to get music embedded from soundcloud using soundcloud API ; however, in order to get users to be able to like and comment on the songs from my web app I need to register my app with soundcloud and get a client id. Soundcloud is currently not accepting new registrations so functionality will be limited to listening to just listening to audio.

--- BandPromotionPlatform/Controllers/HomeController.cs
+++ BandPromotionPlatform/Controllers/HomeController.cs
@@ -15,10 +15,7 @@ namespace BandPromotionPlatform.Controllers
         {
             return View();
         }
-        public IActionResult MusicVideos()
-        {
-            return View();
-        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
---
+++ BandPromotionPlatform/SoundCloud.js
@@ -0,0 +1,44 @@
+﻿var SC = require('soundcloud');
+
+SC.initialize({
+    client_id: 'ssurbancic',
+    redirect_uri: 'http://example.com/callback'
+});
+
+SC.get('users/turkishchrist').then(function (user) { console.log('turkishchrist', user); });
+SC.get('tracks/drunktextingyou-prod-by-mikerobsears').then(function (user) { console.log('DrunkTextingYou [Prod. By MikeRobSears]', user); }).catch();
+SC.get('tracks/drunktextingyou-prod-by-mikerobsears').then(function (user) { console.log('DrunkTextingYou [Prod. By MikeRobSears]', user); }).catch(function (error) {
+    console.log('catch', error);
+});
+var connect = function () {
+    SC.connect().then(function (options) { console.log('success', options); }).catch(function (op) { console.log('error', op); })
+};
+document.getElementById('connect').addEventListener('click', connect);
+var oembedElement = document.getElementById('oembed');
+SC.oEmbed('https://soundcloud.com/turkishchrist/daydreamingaboutus-prod-by-mikerobsears', { element: oembedElement }).then(function (result) {
+    console.log('oembed', result);
+}).catch(function (err) {
+    console.log('oembed err', err);
+    });
+
+SC.connect().then(function () {
+    return SC.put('/me/followings/183');
+}).then(function (user) {
+    alert('You are now following ' + user.username);
+}).catch(function (error) {
+    alert('Error: ' + error.message);
+    });
+
+SC.put('/me', {
+    user: { description: 'I am using the SoundCloud API!' }
+}).then(function () {
+    return SC.get('/me');
+}).then(function (me) {
+    console.log(me.description);
+}).catch(function (error) {
+    alert('Error: ' + error.message);
+    });
+
+SC.stream('/tracks/293').then(function (player) {
+    player.play();
+});
