e62812357505bb792accd67a5f9b527a2f776882
Sam <SSUrbancic@outlook.com>
2018-08-21 15:28:39 -0500

    Updated Event Model
    Updated Event Model with adress city and zip code as well as updated display names

--- BandPromotionPlatform/Models/Event.cs
+++ BandPromotionPlatform/Models/Event.cs
@@ -12,9 +12,21 @@ namespace BandPromotionPlatform.Models
         public int EventID { get; set; }
         public string Subject { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Start Time")]
         public DateTime Start { get; set; }
+        [Display(Name = "End Time")]
         public DateTime End { get; set; }
+        [Display(Name = "Address Line 1")]
+        public string AddressLine1 { get; set; }
+        [Display(Name = "Address Line 2")]
+        public string AddessLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
+        [Display(Name = "Theme Color")]
         public string ThemeColor { get; set; }
+        [Display(Name = "Full Day")]
         public bool IsFullDay { get; set; }
     }
 }
