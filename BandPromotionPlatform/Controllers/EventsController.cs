87e5672b39629acaed42bfdbeb9d91bb1c0a66ec
Sam <SSUrbancic@outlook.com>
2018-08-22 00:20:45 -0500

    GoogleMaps API
    Figured out how to pull coordinates using ajax from google location service. Can't seem to figure out how to pass data to the maps. generated view for details on the event, it will display the address and description.

--- BandPromotionPlatform/Controllers/EventsController.cs
+++ BandPromotionPlatform/Controllers/EventsController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BandPromotionPlatform.Data;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BandPromotionPlatform.Data;
+using BandPromotionPlatform.Models;
 using Newtonsoft.Json;
 
 namespace BandPromotionPlatform.Controllers
@@ -17,14 +19,6 @@ namespace BandPromotionPlatform.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult EventInfo()
-        {
-            return View();
-        }
         public JsonResult GetEvents()
         {
             using (_context)
@@ -33,6 +27,153 @@ namespace BandPromotionPlatform.Controllers
                 var JsonRequestBehavior = new JsonSerializerSettings();
                 return new JsonResult(events, JsonRequestBehavior);
             }
+        }
+        public ActionResult _PartialOnClickEventInfo(int? id)
+        {
+            Event thisEvent = _context.Event.Where(x => x.EventID == id).Select(x => x).First();
+            //string addressLineOneFormated = thisEvent.AddressLine1.Replace(' ', '+');
+            //string addressLineTwoFormated = (!string.IsNullOrEmpty(thisEvent.AddessLine2)) ? thisEvent.AddessLine2.Replace(' ', '+') : null;
+            //string streetAddress = (!string.IsNullOrEmpty(addressLineTwoFormated)) ? addressLineOneFormated + "+" + addressLineTwoFormated : addressLineOneFormated;
+            //string formattedCity = thisEvent.City.Replace(' ', '+');
+            //string formattedAddressComplete = streetAddress + "," + formattedCity + "," + thisEvent.State + "," + thisEvent.ZipCode.ToString();
+            //ViewBag.Address = formattedAddressComplete;
+            var formattedAddressComplete = thisEvent.AddressLine1 + " " + thisEvent.AddessLine2 + " " + thisEvent.City + " " + thisEvent.State + " " + thisEvent.ZipCode;
+            ViewBag.Address = formattedAddressComplete;
+            return View(thisEvent);
+
+        }
+        //GET: Events
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Event.ToListAsync());
+        }
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+        // GET: Events/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .FirstOrDefaultAsync(m => m.EventID == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventID,Subject,Description,Start,End,AddressLine1,AddessLine2,City,State,ZipCode,ThemeColor,IsFullDay")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventID,Subject,Description,Start,End,AddressLine1,AddessLine2,City,State,ZipCode,ThemeColor,IsFullDay")] Event @event)
+        {
+            if (id != @event.EventID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(@event.EventID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .FirstOrDefaultAsync(m => m.EventID == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @event = await _context.Event.FindAsync(id);
+            _context.Event.Remove(@event);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Event.Any(e => e.EventID == id);
         }
     }
 }
